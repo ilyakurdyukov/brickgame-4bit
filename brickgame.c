84c47983ad1455c6c95693ab2af5ca6a6203deee
Ilya Kurdyukov <59548320+ilyakurdyukov@users.noreply.github.com>
2023-12-23 00:39:21 +0700

    bump _XOPEN_SOURCE to 600
    Presumably this will help build for MacOS: https://github.com/ilyakurdyukov/brickgame-4bit/pull/1

--- brickgame.c
+++ brickgame.c
@@ -13,7 +13,7 @@
  * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
-#define _XOPEN_SOURCE 500
+#define _XOPEN_SOURCE 600
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
